137f71bfa6a09504d4901c0bb96881adc39a3a70
sunshine <yeahsmile@users.noreply.github.com>
2015-04-22 01:44:42 +0800

    Update douban.py

--- douban/spiders/douban.py
+++ douban/spiders/douban.py
@@ -10,9 +10,7 @@ class DoubanSpider(CrawlSpider) :
     allowed_domains = ["movie.douban.com"]
     start_urls = ["http://movie.douban.com/top250"]
     rules = (
-        #将所有符合正则表达式的url加入到抓取列表中
         Rule(SgmlLinkExtractor(allow = (r'http://movie\.douban\.com/top250\?start=\d+&filter=&type=',))),
-        #将所有符合正则表达式的url请求后下载网页代码, 形成response后调用自定义回调函数
         Rule(SgmlLinkExtractor(allow = (r'http://movie\.douban\.com/subject/\d+', )), callback = 'parse_page', follow = True),
         )
 
